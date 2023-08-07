1a2def02f2bec2cd873d95d86f02467b056ade84
tetejiro <hy1733505@gmail.com>
2023-08-08 01:01:10 +0900

    $emit

--- resources/js/Pages/ComponentTest.vue
+++ resources/js/Pages/ComponentTest.vue
@@ -2,11 +2,13 @@
   import GuestLayout from '@/Layouts/GuestLayout.vue';
   import InputLabel from '@/Components/InputLabel.vue';
   import TextInput from '@/Components/TextInput.vue';
+
+  const emitTest = val => console.log(val);
 </script>
 
 <template>
   <GuestLayout>
     <InputLabel value="a"></InputLabel>
-    <TextInput modelValue="modelValue"></TextInput>
+    <TextInput modelValue="modelValue" @update:modelValue="emitTest"></TextInput>
   </GuestLayout>
 </template>
