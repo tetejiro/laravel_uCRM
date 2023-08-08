487e28f422cd82d398fa8e6d4dc51c9e9fca117e
tetejiro <hy1733505@gmail.com>
2023-08-08 12:33:09 +0900

    Items シーディングファイル

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ItemSeeder extends Seeder
 {
@@ -14,6 +15,22 @@ class ItemSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('Items')->insert([
+            [
+                'name' => 'カット',
+                'memo' => 'カット詳細',
+                'price' => 6000
+            ],
+            [
+                'name' => 'カラー',
+                'memo' => 'カラー詳細',
+                'price' => 8000
+            ],
+            [
+                'name' => 'パーマ',
+                'memo' => 'パーマ詳細',
+                'price' => 13000
+            ]
+        ]);
     }
 }
